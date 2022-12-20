cb9e0d88cbec74cce58d964abb77918cc81b4dd7
Srijan R Shetty <srijan.shetty@gmail.com>
2022-12-20 11:34:07 +0530

    feat: simple tsnet app

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+	"strings"
+
+	"tailscale.com/tsnet"
+)
+
+var (
+	hostname = flag.String("hostname", "hello", "hostname for the tailnet")
+)
+
+func main() {
+	flag.Parse()
+
+	s := &tsnet.Server{
+		Hostname: *hostname,
+	}
+
+	defer s.Close()
+
+	ln, err := s.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer ln.Close()
+
+	lc, err := s.LocalClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Fatal(http.Serve(ln, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		who, err := lc.WhoIs(r.Context(), r.RemoteAddr)
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		fmt.Fprintf(w, "<html><body><h1>Hello, world!</h1>\n")
+		fmt.Fprintf(w, "<p>You are <b>%s</b> from <b>%s</b> (%s)</p>",
+			html.EscapeString(who.UserProfile.LoginName),
+			html.EscapeString(firstLabel(who.Node.ComputedName)),
+			r.RemoteAddr)
+	})))
+}
+
+func firstLabel(s string) string {
+	if hostname, _, ok := strings.Cut(s, "."); ok {
+		return hostname
+	}
+
+	return s
+}
+
